47d8b65d82035972623880041b11e7b9a951da47
valimbavaka <christopheric301@gmail.com>
2025-06-12 20:08:30 +0300

    Reglage du popup d' ajout

--- Frontend/src/Views/Pages/HomePage.jsx
+++ Frontend/src/Views/Pages/HomePage.jsx
@@ -18,26 +18,28 @@ export default function HomePage() {
 
 
     return (
-        <div className="home-page">
-            <div className="section-container animate__animated animate__fadeInLeft">
-                <AddModal visible={modal} hideModal={hideModal}/>
-                <div className="section-text">
+        <>
+            <div className="home-page">
+                <div className="section-container animate__animated animate__fadeInLeft">
+                    <div className="section-text">
                 <span>
                     <span style={{fontSize: "35px", letterSpacing: "2px"}} className="fw-bolder">
                         Visualiser les statistiques des salaires
                     </span>
                 </span>
-                    <p style={{ fontSize: "13px" }}>
-                        Ce site vous offre la possibilité d'enregistrer vos salariés et
-                        avoir une idée des salaires minimaux ainsi que maximaux
-                        à travers des données statistiques.
-                    </p>
-                    <button onClick={showModal} className="btn-add animate__animated animate__fadeInDown " style={{ animationDelay : "0.5s" }} >Nouveau employé</button>
+                        <p style={{ fontSize: "13px" }}>
+                            Ce site vous offre la possibilité d'enregistrer vos salariés et
+                            avoir une idée des salaires minimaux ainsi que maximaux
+                            à travers des données statistiques.
+                        </p>
+                        <button onClick={showModal} className="btn-add animate__animated animate__fadeInDown " style={{ animationDelay : "0.5s" }} >Nouveau employé</button>
+                    </div>
+                </div>
+                <div className="image__panel animate__animated animate__fadeInUp  " style={{ animationDelay : "0.6s" }} >
+                    <HomePanel/>
                 </div>
             </div>
-            <div className="image__panel animate__animated animate__fadeInUp  " style={{ animationDelay : "0.6s" }} >
-                <HomePanel/>
-            </div>
-        </div>
+            <AddModal class="add-modal" visible={modal} hideModal={hideModal}/>
+        </>
     );
 }
